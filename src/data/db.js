b64a69139f2dbd64f29f565bcca7751d4e3c13f7
ShiellynFerr <brunashiellyn@gmail.com>
2022-08-09 11:30:08 -0300

    banco mocado

---
+++ src/data/db.js
@@ -0,0 +1,20 @@
+ const db = {
+  tips:[
+    'Faça atividade física',
+    'Alimente-se bem',
+    'Tenha autoconfiança',
+    'Planeje seu dia antes de dormir',
+    'Faça uma tarefa por vez',
+    'Priorize suas responsabilidades',
+    'Faça listas',
+    'Tenha uma boa noite de sono',
+    'Cuide da sua saúde física e mental',
+    'Cultive bons relacionamentos',
+    'Pratique diariamente',
+    'Inicie com o básico',
+    'Seja paciente com você mesmo',
+    'Tire férias'
+  ],
+}
+
+export default db;
